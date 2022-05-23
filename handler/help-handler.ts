4b5a441dcbcc11fc535edef32b99a5a62774f135
thoriqadillah <thoriqadillah59@gmail.com>
2022-05-23 11:25:18 +0700

    change for handling help response

---
+++ handler/help-handler.ts
@@ -0,0 +1,14 @@
+export default function buildHelpResponse() {
+    console.log(
+        'SIAM Bot by thoriqadillah\n\n' +
+    
+        'flags\n' +
+        '  -h --help        Menampilkan help\n' +
+        '  -u --username    Input username (wajib diisi)\n' +
+        '  -p --password    Input password (wajib diisi)\n' +
+        '  -s --slowMo      Set slow motion bot. Default 100ms (0.9x)\n' +
+        '  -r --random      Set randomisasi jawaban kuesioner. Default 3 (Normal, Baik, Sangat Baik) \n' 
+    )
+}
+
+    
