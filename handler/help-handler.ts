761bdada3832d696988b9023621e83c86cbf8036
thoriqadillah <thoriqadillah59@gmail.com>
2022-05-23 12:02:55 +0700

    changing help builder response

--- handler/help-handler.ts
+++ handler/help-handler.ts
@@ -5,9 +5,13 @@ export default function buildHelpResponse() {
         'flags\n' +
         '  -h --help        Menampilkan help\n' +
         '  -u --username    Input username (wajib diisi)\n' +
-        '  -p --password    Input password (wajib diisi)\n' +
+        '  -p --password    Input password (wajib diisi)\n\n' +
+
+        'optional\n' +
         '  -s --slowMo      Set slow motion bot. Default 100ms (0.9x)\n' +
-        '  -r --random      Set randomisasi jawaban kuesioner. Default 3 (Normal, Baik, Sangat Baik) \n' 
+        '  -r --random      Set randomisasi jawaban kuesioner. Default 3 (Normal, Baik, Sangat Baik) \n' +
+        '  -e --exclude     Exclude dosen agar dapat Anda isi kuesioner secara manual\n' 
+
     )
 }
 
