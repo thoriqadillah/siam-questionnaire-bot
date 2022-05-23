b2d6a88339a6731966e0cc0d2d2009381dd20b23
thoriqadillah <thoriqadillah59@gmail.com>
2022-05-23 12:28:17 +0700

    adding color to required input

--- handler/required-handler.ts
+++ handler/required-handler.ts
@@ -1,4 +1,6 @@
+import * as Colors from "https://deno.land/std/fmt/colors.ts"
+
 export default function throwRequiredException() {
-    console.log('username dan password wajib diisi')
+    console.log(Colors.red('username dan password wajib diisi'))
 }
 
